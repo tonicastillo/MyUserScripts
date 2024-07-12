ee3a5dfd28b88fdc0fb10ccac3aa99e0e005ae9a
tonicastillo <tonicastillo@gmail.com>
2024-07-12 19:22:05 +0200

    multiple servers

--- OwnToneNFCSaveCardButton.user.js
+++ OwnToneNFCSaveCardButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OwnTone NFC Save Card Button
 // @namespace    
-// @version      0.3
+// @version      0.4
 // @description  
 // @author       Toni Castillo
 // @match        http://192.168.1.152:3679/*
@@ -12,21 +12,38 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+const servers = {
+    '192.168.1.152:3679': {
+        musicboxUrl: 'http://musicboxsalon.local/'
+    },
+    '192.168.1.152:3689': {
+        musicboxUrl: 'http://musicboxestudio.local/'
+    },
+    '192.168.1.152:3699': {
+        musicboxUrl: 'http://musicboxchildren.local/'
+    }'
+}
 
 
-        var intv = setInterval(function() {
-                var hash = window.location.hash
+    (function () {
+        'use strict';
+        let musicboxUrl = ''
+        for (let server in servers) {
+            if (window.location.host === server) musicboxUrl = servers[server].musicboxUrl
+        }
+
+
+        var intv = setInterval(function () {
+            var hash = window.location.hash
             var container = document.querySelector('h1.title')
             var button = document.querySelector('a.nfcmusicboxbutton')
-            if(hash.substr(0,15) === '#/music/albums/' && container && !button){
+            if (hash.substr(0, 15) === '#/music/albums/' && container && !button) {
                 var albumId = hash.substr(15)
 
                 var b = document.createElement('a');
-                b.href = "http://musicboxsalon.local/#"+albumId
+                b.href = musicboxUrl + "#" + albumId
                 b.target = "_blank"
-                b.className="nfcmusicboxbutton"
+                b.className = "nfcmusicboxbutton"
                 b.innerHTML = "Save NFC"
                 b.style.display = 'block'
                 b.style.color = '#485fc7'
@@ -38,4 +55,4 @@
 
         }, 500);
 
-})();
+    })();
